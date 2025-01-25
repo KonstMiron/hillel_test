6e3c2d773b61248c690dac2c80aac3689d2949fd
Miron <myroshnychenk0@icloud.com>
2025-01-25 01:14:57 +0100

    Add file

--- src/main.js
+++ src/main.js
@@ -1,30 +1,36 @@
-// Homework 7
+// Homework 8
 //Zadanie 1
-function reverseArray(array) {
-    return array.slice().reverse();
-}
-const originalArray = [1, 2, 3, 4, 5];
-const reversedArray = reverseArray(originalArray);
-
-console.log(reversedArray); // [5, 4, 3, 2, 1]
+function capitalizeStrings(strings) {
+    return strings.map(str => {
+      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    });
+  }
+  
+  const words = ["apple", "banaNA", "kiWi", "ORANGE"];
+  console.log(capitalizeStrings(words)); // ["Apple", "Banana", "Kiwi", "Orange"]
 //Zadanie 2
-function uniqueValues(array1, array2) {
-    return Array.from(new Set([...array1, ...array2]));
-}
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-const uniqueValuesArray = uniqueValues(array1, array2);
-
-console.log(uniqueValuesArray); // [1, 2, 3, 4, 5, 6, 7]
+function findCommonElements(arr1, arr2) {
+    return arr1.filter(element => arr2.includes(element));
+  }
+  
+  const array1 = [1, 2, 3, 4, 5];
+  const array2 = [3, 4, 5, 6, 7];
+  
+  console.log(findCommonElements(array1, array2)); // [3, 4, 5]
 // Zadanie 3
-function calculateAverageGrade(students) {
-    const totalGrade = students.reduce((sum, student) => sum + student.grade, 0);
-    return Math.round((totalGrade / students.length) * 10) / 10;
-}
-const students = [
-    { name: "Alice", age: 20, grade: 4.5 },
-    { name: "Bob", age: 21, grade: 3.9 },
-    { name: "Charlie", age: 19, grade: 4.8 }
-];
-
-console.log(calculateAverageGrade(students)); // 4.4
+function analyzeArray(numbers) {
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    const average = sum / numbers.length;
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+  
+    return {
+      sum,
+      average,
+      min,
+      max
+    };
+  }
+  
+  const numbers = [1, 2, 3, 4, 5];
+  console.log(analyzeArray(numbers)); // { sum: 15, average: 3, min: 1, max: 5 }
